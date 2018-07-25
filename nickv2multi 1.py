2b6058ab8f6d5a8decbc2b20fa70a84b60c4e92b
hyperwing <drdavidwing@gmail.com>
2018-07-25 17:13:27 -0500

    multithread file

---
+++ nickv2multi 1.py
@@ -0,0 +1,26 @@
+safeware - CDW contacts
+value added reseller
+
+value of equipment is high
+
+service contract 
+on demand repair capability - kiosk
+
+soluto augments IT, not sell through
+add on campus support - anywhere expert/remote support
+
+Josh Baker
+
+Pricing = student discount
+
+soluto costs $6-7 for accidental damage
+roll into tuition is good => benefit for school?
+
+top tech colleges -> bundle with tech store
+
+find out how much interest is in student industry 
+
+monthly subscription - more customer engagment
+
+champions for asurion
+
